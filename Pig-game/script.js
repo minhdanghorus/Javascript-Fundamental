3c0e90fccfc4e543c32e1ffa368964044fedaf14
minhdangnsp <dangnguyentranminh@gmail.com>
2025-12-14 15:38:20 +0700

    add: Holing current score

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -20,9 +20,20 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
+  if (!playing) return;
+
   // 1. generate a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
   // 2. display dice
@@ -37,10 +48,32 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (!playing) return;
+
+  // 1. add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. check if player's score is >= 100
+  if (scores[activePlayer] >= 20) {
+    // Finish the game
+    playing = false;
+    diceEl.classList.add('hidden');
+
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    // 3. switch to the next player
+    switchPlayer();
   }
 });
